a159e48e3e3d5418a656bc028c20787f1ca4cada
Sagar <Sagararyal1122@gmail.com>
2022-12-24 23:50:02 +0545

    Json pars

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -1,6 +1,12 @@
+import 'dart:convert';
+
+import 'package:apicall/models/model.dart';
+import 'package:apicall/models/usermodel.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+
+import 'package:http/http.dart' as http;
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,38 +16,81 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  List<Fulldetails> fulldetails = [];
+  @override
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-          child: Column(
-        children: [
-          Row(
-            children: [
-              Expanded(
-                child: Container(
-                  child: Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          "The use User ID one",
-                          style: TextStyle(fontSize: 30),
+      body: FutureBuilder(
+        future: getData(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: fulldetails.length,
+              itemBuilder: (context, index) {
+                return Row(
+                  children: [
+                    Expanded(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          child: Padding(
+                            padding: const EdgeInsets.all(15),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.center,
+                              children: [
+                                // GetText(index, 'ID',
+                                //     fulldetails[index].id.toString()),
+
+                                GetText(
+                                    index,
+                                    fulldetails[index].name,
+                                    fulldetails[index].address.street,
+                                    fulldetails[index].address.geo.lat)
+                              ],
+                            ),
+                          ),
+                          color: Colors.pink,
+                          height: 200,
                         ),
-                        Text("The Is Title", style: TextStyle(fontSize: 30)),
-                        Text("TheIS IS BODY", style: TextStyle(fontSize: 30)),
-                      ],
+                      ),
                     ),
-                  ),
-                  color: Colors.pink,
-                  height: 200,
-                ),
-              ),
-            ],
-          )
-        ],
-      )),
+                  ],
+                );
+              },
+            );
+          }
+          // return CircularProgressIndicator();
+          return Container();
+        },
+      ),
     );
+  }
+
+  Column GetText(
+      int index, String fieldname, String content, String ajaiaauta) {
+    return Column(
+      children: [
+        Text(fieldname),
+        Text(fieldname),
+        Text(content),
+        Text(ajaiaauta)
+      ],
+    );
+  }
+
+  final url = "http://jsonplaceholder.typicode.com/users";
+  Future<List<Fulldetails>> getData() async {
+    final responce = await http.get(Uri.parse(url));
+    var data = jsonDecode(responce.body.toString());
+    if (responce.statusCode == 200) {
+      print("Hit Sucessfully");
+      print(fulldetails.length);
+
+      for (Map<String, dynamic> index in data) {
+        fulldetails.add(Fulldetails.fromJson(index));
+      }
+    } else {}
+    return fulldetails;
   }
 }
